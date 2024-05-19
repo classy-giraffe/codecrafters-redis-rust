9cd15dabb19652af94fce18f9802717533f4353c
Tommaso Chiti <chitilivorno@gmail.com>
2024-05-19 12:38:31 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::io;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    let mut buffer = [0; 512]; // Use a fixed-size buffer for reading
+
+    loop {
+        let bytes_read = stream.read(&mut buffer)?;
+    
+        if bytes_read == 0 {
+            break; // Connection closed
+        }
+    
+        stream.write_all(b"+PONG\r\n")?;
+        stream.flush()?;
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
